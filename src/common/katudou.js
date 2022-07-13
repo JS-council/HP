ae91f25cfec1e341b706f2a9e5219d93cbc47c09
Mimata8236 <109130983+Mimata8236@users.noreply.github.com>
2022-07-13 16:42:33 +0900

    Update katudou.js

--- src/common/katudou.js
+++ src/common/katudou.js
@@ -5,14 +5,17 @@ export default {
       activity: [
         {
           month: '6',
+          color: '#1E88E5',
           fileName: '202206'
         },
         {
           month: '5',
+          color: '#388E3C',
           fileName: '202205'
         },
         {
           month: '4',
+          color: '#F57C00',
           fileName: '202204'
         }
       ]
